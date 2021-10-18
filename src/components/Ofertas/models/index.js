5ce5264d297888f07ef8a256baa1688af27a6103
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-10-18 18:43:45 -0500

    ADD: Termina modelos para programacion de ofertas

--- src/components/Ofertas/models/index.js
+++ src/components/Ofertas/models/index.js
@@ -153,30 +153,112 @@ const modelsOfertas = (() => {
         }
     }
 
-    const deleteMasterOffer = async (cadenaConexion = '', uuid, bodyMaster) => {
+    const deleteMasterOffer = async (cadenaConexion = '', uuid) => {
+        try {
+            const accessToDataBase = dbpostgres.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `DELETE FROM maestroofertas WHERE uuid = '${uuid}'`,
+                QueryTypes.DELETE
+            );
+            dbpostgres.closeConexion();
+            return createContentAssert('El maestro ofertas ha sido eliminado con exito', result);
+        } catch (error) {
+            console.log(error);
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar eliminar la oferta maestro',
+                error
+            );
+        }
+    }
+
+    const getOffersByMasterOffer = async (cadenaConexion = '', uuid) => {
+        try {
+            const accessToDataBase = dbpostgres.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `SELECT * FROM articulosofertas WHERE uuid_maestro = '${uuid}'`,
+                QueryTypes.SELECT
+            );
+            dbpostgres.closeConexion();
+            return createContentAssert('Datos encontrados en la base de datos', result[0]);
+        } catch (error) {
+            console.log(error);
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar obtener los articulos de una oferta',
+                error
+            );
+        }
+    }
+
+    const createOffers = async (cadenaConexion = '', bodyMaster) => {
         try {
             const {
-                status, editable, tipoOferta, fechaInicio, fechaFin,
-                descripcion, fechaModificado, modificadoPor
+                uuid_maestro, articulo, nombre, costo, descripcion, precio, oferta,
+                fechaAlta, creadoPor, fechaModificado, modificadoPor
             } = bodyMaster
             const accessToDataBase = dbpostgres.getConexion(cadenaConexion);
             const result = await accessToDataBase.query(
+            `INSERT INTO articulosofertas VALUES(
+                '${uuid_maestro}', '${articulo}', '${nombre}', ${costo}, '${descripcion}', ${precio},
+                ${oferta}, '${fechaAlta}', '${creadoPor}', '${fechaModificado}', '${modificadoPor}'
+            )`,
+                QueryTypes.INSERT
+            );
+            dbpostgres.closeConexion();
+            return createContentAssert('Articulo agregado a la oferta', result);
+        } catch (error) {
+            console.log(error);
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar agregar un articulo a la oferta',
+                error
+            );
+        }
+    }
+
+    const updateOffer = async (cadenaConexion = '', articulo, bodyArticulo) => {
+        try {
+            const {
+                nombre, costo, descripcion, precio, oferta,
+                descripcion, fechaModificado, modificadoPor
+            } = bodyArticulo
+            const accessToDataBase = dbpostgres.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
                 `
-                UPDATE maestroofertas
+                UPDATE articulosofertas
                 SET
-                    status = ${status}, Editable = ${editable}, TipoOferta = '${tipoOferta}',
-                    FechaInicio = '${fechaInicio}', FechaFin = '${fechaFin}', Descripcion = '${descripcion}',
+                    nombre = '${nombre}', costo = ${costo}, descripcion = '${descripcion}',
+                    precio = ${precio}, oferta = ${oferta},
                     fechaModificado = '${fechaModificado}', modificadoPor= '${modificadoPor}'
-                WHERE uuid = '${uuid}'
+                WHERE articulo = '${articulo}'
                 `,
                 QueryTypes.UPDATE
             );
             dbpostgres.closeConexion();
-            return createContentAssert('Datos de la maestro oferta ha sido actualizado', result);
+            return createContentAssert('Datos del articulo han sido actualizados', result);
         } catch (error) {
             console.log(error);
             return createContentError(
-                'Fallo la conexion con base de datos al intentar modificar los datos de la oferta maestro',
+                'Fallo la conexion con base de datos al intentar modificar los datos del articulo',
+                error
+            );
+        }
+    }
+
+    const deleteOffer = async (cadenaConexion = '', articulo) => {
+        try {
+            const accessToDataBase = dbpostgres.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `
+                DELETE FROM articulosofertas
+                WHERE uuid_maestro = '${uuid_maestro}' AND articulo = '${articulo}'
+                `,
+                QueryTypes.DELETE
+            );
+            dbpostgres.closeConexion();
+            return createContentAssert('El articulo de la oferta ha sido eliminado con exito', result);
+        } catch (error) {
+            console.log(error);
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar eliminar el articulo de la oferta',
                 error
             );
         }
@@ -189,6 +271,11 @@ const modelsOfertas = (() => {
         createMasterOffers,
         updateStatusMasterOffer,
         updateDataMasterOffer,
+        deleteMasterOffer,
+        getOffersByMasterOffer,
+        createOffers,
+        updateOffer,
+        deleteOffer,
     }
 })();
 
