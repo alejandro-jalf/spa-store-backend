b0094e821feb5905c2ad624d0d1669ad6ae56ac1
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-10-18 19:21:06 -0500

    ADD: Agrega validaciones para ofertas

--- src/components/Ofertas/schemas/index.js
+++ src/components/Ofertas/schemas/index.js
@@ -1,9 +1,63 @@
 const joi = require('joi');
 
 const schemasOfertas = (() => {
-    // Declaracion de esquemas
-    
-    return {}
+    // 2021-08-24T05:00:00.000z
+    const schemaDate = joi.string().regex(/^\d{4,4}-\d{2,2}-\d{2,2}T\d{2,2}:\d{2,2}:\d{2,2}.000z$/);
+
+    const schemaCreateMasterOffer = joi.object({
+        sucursal: joi.string().max(10).min(2).required(),
+        status: joi.number().min(0).max(7).required(),
+        editable: joi.boolean().required(),
+        tipoOferta: joi.string().min(2).required(),
+        fechaInicio: joi.string().min(2).required(),
+        fechaFin: joi.string().min(2).required(),
+        descripcion: joi.string().empty().required(),
+        creadoPor: joi.string().required(),
+    })
+
+    const schemaUpdateMasterOffer = joi.object({
+        status: joi.number().min(0).max(7),
+        editable: joi.boolean(),
+        tipoOferta: joi.string().min(2),
+        fechaInicio: joi.string().min(2),
+        fechaFin: joi.string().min(2),
+        descripcion: joi.string().empty(),
+        modificadoPor: joi.string().required(),
+    })
+
+    const schemaUpdateStatusMasterOffer = joi.object({
+        status: joi.number().min(0).max(7).required(),
+        modificadoPor: joi.string().required(),
+    })
+
+    const schemaCreateArticle = joi.object({
+        uuid_maestro: joi.string().min(2).required(),
+        articulo: joi.string().min(1).required(),
+        nombre: joi.string().min(2).required(),
+        costo: joi.number().required(),
+        descripcion: joi.string().min(2).required(),
+        precio: joi.number().required(),
+        oferta: joi.number().required(),
+        creadoPor: joi.string().min(2).required(),
+    })
+
+    const schemaUpdateArticle = joi.object({
+        nombre: joi.string().min(2),
+        costo: joi.number(),
+        descripcion: joi.string().min(2),
+        precio: joi.number(),
+        oferta: joi.number(),
+        modificadoPor: joi.string().min(2).required(),
+    })
+
+    return {
+        schemaUpdateStatusMasterOffer,
+        schemaCreateMasterOffer,
+        schemaUpdateMasterOffer,
+        schemaDate,
+        schemaCreateArticle,
+        schemaUpdateArticle,
+    }
 })() ;
 
 module.exports = schemasOfertas;
--- src/components/Ofertas/validations/index.js
+++ src/components/Ofertas/validations/index.js
@@ -1,4 +1,11 @@
-const { schemaCodigoBarras } = require('../schemas/index');
+const { 
+    schemaCreateArticle,
+    schemaCreateMasterOffer,
+    schemaDate,
+    schemaUpdateArticle,
+    schemaUpdateMasterOffer,
+    schemaUpdateStatusMasterOffer,
+} = require('../schemas');
 const { createContentAssert, createContentError } = require('../../../utils');
 
 const validationOfertas = (() => {
@@ -13,8 +20,81 @@ const validationOfertas = (() => {
         return createContentAssert('Sucursal valida');
     }
 
+    const validateBodyCreateMasterOffer = (bodyCreateMaster) => {
+        let resultValidate = schemaCreateMasterOffer.validate(bodyCreateMaster);
+        if (resultValidate.error)
+            return createContentError('Algun dato fue enviado de manera incorrecta', resultValidate.error)
+        
+        resultValidate = schemaDate.validate(bodyCreateMaster.fechaInicio);
+        if (resultValidate.error)
+            return createContentError(
+                'La fecha de inicio no tiene el formato correcto 0000-00-00T00:00:00.000z',
+                resultValidate.error
+            )
+        
+        resultValidate = schemaDate.validate(bodyCreateMaster.fechaFin);
+        if (resultValidate.error)
+            return createContentError(
+                'La fecha de termino no tiene el formato correcto 0000-00-00T00:00:00.000z',
+                resultValidate.error
+            )
+        
+        return createContentAssert('Datos validos');
+    }
+
+    const validateBodyUpdateMasterOffer = (bodyUpdateMaster) => {
+        let resultValidate = schemaUpdateMasterOffer.validate(bodyUpdateMaster);
+        if (resultValidate.error)
+            return createContentError('Algun dato fue enviado de manera incorrecta', resultValidate.error)
+        
+        resultValidate = schemaDate.validate(bodyUpdateMaster.fechaInicio);
+        if (resultValidate.error)
+            return createContentError(
+                'La fecha de inicio no tiene el formato correcto 0000-00-00T00:00:00.000z',
+                resultValidate.error
+            )
+        
+        resultValidate = schemaDate.validate(bodyUpdateMaster.fechaFin);
+        if (resultValidate.error)
+            return createContentError(
+                'La fecha de termino no tiene el formato correcto 0000-00-00T00:00:00.000z',
+                resultValidate.error
+            )
+        
+        return createContentAssert('Datos validos');
+    }
+    
+    const validateBodyUpdateStatusMasterOffer = (bodyMaster) => {
+        let resultValidate = schemaUpdateStatusMasterOffer.validate(bodyMaster);
+        if (resultValidate.error)
+            return createContentError('Algun dato fue enviado de manera incorrecta', resultValidate.error)
+
+        return createContentAssert('Datos validos');
+    }
+    
+    const validateBodyCreateArticle = (bodyArticle) => {
+        let resultValidate = schemaCreateArticle.validate(bodyArticle);
+        if (resultValidate.error)
+            return createContentError('Algun dato fue enviado de manera incorrecta', resultValidate.error)
+
+        return createContentAssert('Datos validos');
+    }
+    
+    const validateBodyUpdateArticle = (bodyArticle) => {
+        let resultValidate = schemaUpdateArticle.validate(bodyArticle);
+        if (resultValidate.error)
+            return createContentError('Algun dato fue enviado de manera incorrecta', resultValidate.error)
+
+        return createContentAssert('Datos validos');
+    }
+
     return {
         validateSucursal,
+        validateBodyCreateMasterOffer,
+        validateBodyUpdateMasterOffer,
+        validateBodyUpdateStatusMasterOffer,
+        validateBodyCreateArticle,
+        validateBodyUpdateArticle,
     }
 })();
 
