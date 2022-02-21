dbb0a64630640c7f27b3de9b535f1118b455dddb
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2022-02-21 14:01:28 -0600

    ADD: Agrega solo rutas de existencias articulos

--- src/components/Articulos/routers/index.js
+++ src/components/Articulos/routers/index.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
-const { getPriceArticle, getDataForStocks } = require("../services");
+const {
+    getPriceArticle,
+    getDataForStocks,
+    getExistenciasByNombre,
+    getDetallesExistenciasBySku,
+} = require("../services");
 
 router.route("/api/v1/articulos/:articulo/precio").get(async (req, res) => {
     const { articulo } = req.params;
@@ -11,6 +16,19 @@ router.route("/api/v1/articulos/:articulo/precio").get(async (req, res) => {
 router.route("/api/v1/articulos/stocks").get(async (req, res) => {
     const { sucursal, company, daymin, daymax } = req.query;
     const { status, response } = await getDataForStocks(sucursal, company, daymin, daymax);
+    res.status(status).json(response);
+});
+
+router.route("/api/v1/articulos/:nombre/existencias").get(async (req, res) => {
+    const { nombre } = req.params;
+    const { status, response } = await getExistenciasByNombre(nombre);
+    res.status(status).json(response);
+});
+
+router.route("/api/v1/articulos/:sku/existencias/detalles").get(async (req, res) => {
+    const { sku } = req.params;
+    const { sucursal } = req.query;
+    const { status, response } = await getDetallesExistenciasBySku(sucursal, sku);
     res.status(status).json(response);
 });
 
