734795f137a978eda76358ea3b5d8b7e5fa1f5c8
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2022-03-08 11:09:09 -0600

    Crea servicio para codificador de articulos

--- src/components/Articulos/services/index.js
+++ src/components/Articulos/services/index.js
@@ -11,10 +11,15 @@ const {
     validateSucursalWithCompany,
     validateDayMinAndMax,
 } = require('../validations');
-const { getPrecio, calculateStocks, updateStockByScripts } = require('../models');
+const {
+    getPrecio,
+    calculateStocks,
+    updateStockByScripts,
+    getDetailsArticleForCodificador,
+} = require('../models');
 
 const ServicesArticulos = (() => {
-    
+
     const getPriceArticle = async (sucursal, codigoBarras) => {
         let validate = validateSucursal(sucursal);
         if (!validate.success)
@@ -76,9 +81,27 @@ const ServicesArticulos = (() => {
         return createResponse(200, response)
     }
 
+    const getDetallesArticulosByCodificador = async (sucursal, codigoBarras) => {
+        let validate = validateSucursal(sucursal);
+        if (!validate.success)
+            return createResponse(400, validate);
+
+        validate = validateCodigoBarras(codigoBarras);
+        if (!validate.success) return createResponse(400, validate);
+
+        const conexion = getConnectionFrom(sucursal);
+        const response  = await getDetailsArticleForCodificador(conexion, sucursal, codigoBarras);
+
+        if (!response.success) return createResponse(400, response)
+        if (response.data.length === 0)
+            return createResponse(400, createContentError('No se encontro el articulo'))
+        return createResponse(200, response)
+    }
+
     return {
         getPriceArticle,
         getDataForStocks,
+        getDetallesArticulosByCodificador,
     }
 })();
 
