f486639235e67ae991370a040daf6cfc6e0edc49
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2022-03-08 10:57:51 -0600

    ADD: Agrega modelo para codificador de articulos

--- src/components/Articulos/models/index.js
+++ src/components/Articulos/models/index.js
@@ -129,7 +129,43 @@ const modelsArticulos = (() => {
         }
     }
 
+    const getDetailsArticleForCodificador = async (cadenaConexion = '', sucursal = '', article = '') => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `
+                DECLARE @Sucursal NVARCHAR(2) = '${sucursal}'
+                DECLARE @Almacen INT = CASE WHEN @Sucursal = 'ZR' THEN 2 WHEN @Sucursal = 'VC' THEN 3 WHEN @Sucursal = 'OU' THEN 19 WHEN @Sucursal = 'JL' THEN 7 WHEN @Sucursal = 'BO' THEN 21 ELSE 0 END;
+                DECLARE @Tienda INT = CASE WHEN @Sucursal = 'ZR' THEN 1 WHEN @Sucursal = 'VC' THEN 2 WHEN @Sucursal = 'OU' THEN 5 WHEN @Sucursal = 'JL' THEN 4 WHEN @Sucursal = 'BO' THEN 6 ELSE 0 END;
+                DECLARE @articulo NVARCHAR(15) = (SELECT Articulo FROM ArticulosRelacion WHERE CodigoBarras = '${article}' OR Articulo = '${article}');
+                DECLARE @article NVARCHAR(15) = ISNULL(@articulo, '${article}');
+
+                SELECT
+                    A.Nombre, A.CodigoBarras, A.Articulo, A.Descripcion, E.ExistenciaActualRegular,
+                    Relacion = CAST(CAST(A.FactorVenta AS int) AS nvarchar) + A.UnidadVenta + '/' + CAST(CAST(A.FactorCompra AS int) AS nvarchar) + A.UnidadCompra
+                FROM QVListaprecioConCosto AS A
+                INNER JOIN QVExistencias AS E ON A.Articulo = E.Articulo
+                WHERE (A.Articulo = @article OR A.CodigoBarras = @article)
+                    AND A.Tienda = @Tienda
+                    AND A.Almacen = @Almacen
+                    AND E.Tienda = @Tienda
+                    AND E.Almacen = @Almacen
+                `,
+                QueryTypes.SELECT
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Stock actualizados', result[0]);
+        } catch (error) {
+            console.log(error);
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar actualizar los stocks',
+                error
+            );
+        }
+    }
+
     return {
+        getDetailsArticleForCodificador,
         updateStockByScripts,
         calculateStocks,
         getPrecio,
