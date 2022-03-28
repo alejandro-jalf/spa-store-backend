79cd49d4b408335b75136dc21ae595be68ba053b
alejandro-jalf <aleflo_1996@outlook.com>
2022-03-28 10:31:58 -0600

    ADD: Genera rutas para folios

--- src/components/General/routers/index.js
+++ src/components/General/routers/index.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
-const { getStatusConections } = require("../services");
+const {
+    getStatusConections,
+    getCalculateFolios,
+    updateFoliosAvailable,
+} = require("../services");
 
 router.route("/api/v1/general/:empresa/conexiones/activas").get(async (req, res) => {
     const { empresa } = req.params;
     const { status, response } = await getStatusConections(empresa);
+    res.status(status).json(response);
+});
+
+router.route("/api/v1/general/folios/:sucursal").get(async (req, res) => {
+    const { sucursal } = req.params;
+    const { promMensual } = req.query;
+    const { status, response } = await getCalculateFolios(sucursal, promMensual);
+    res.status(status).json(response);
+});
+
+router.route("/api/v1/general/folios/:sucursal").put(async (req, res) => {
+    const { sucursal } = req.params;
+    const { newFolio } = req.query;
+    const { status, response } = await updateFoliosAvailable(sucursal, newFolio);
     res.status(status).json(response);
 });
 
