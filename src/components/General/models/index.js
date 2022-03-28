07d188a78a6106554f8af2e2ef707d9ca6c36f38
alejandro-jalf <aleflo_1996@outlook.com>
2022-03-28 10:32:28 -0600

    ADD: Agrega Modelos para folios

--- src/components/General/models/index.js
+++ src/components/General/models/index.js
@@ -20,8 +20,48 @@ const modelsGeneral = (() => {
         }
     }
 
+    const calculaFoliosSucursal = async (cadenaConexion = '', sucursal = 'ZR', promMensual = 200) => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `SELECT
+                    Tienda,
+                    Serie,
+                    FolioInicial,
+                    FolioFinal,
+                    FolioActual,
+                    FolioDisponible = FolioFinal - FolioActual,
+                    INCREMENTODEFOLIO = ${promMensual} - (FolioFinal - FolioActual),
+                    FOLIOFINC = (${promMensual} - (FolioFinal - FolioActual)) + FOLIOFINAL
+                FROM SeriesFolios
+                WHERE Serie = '${sucursal}'`,
+                QueryTypes.SELECT
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Datos de folios encontrados', result[0]);
+        } catch (error) {
+            return createContentError('Fallo al intentar obtener el calculo de folios mensuales', error);
+        }
+    }
+
+    const updateFoliosSucursal = async (cadenaConexion = '', sucursal = 'ZR', newFolio) => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `UPDATE SeriesFolios SET FolioFinal = ${newFolio} WHERE Serie = '${sucursal}'`,
+                QueryTypes.UPDATE
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Datos de folios encontrados', result[0]);
+        } catch (error) {
+            return createContentError('Fallo al intentar actualizar los folios', error);
+        }
+    }
+
     return {
         testConnection,
+        calculaFoliosSucursal,
+        updateFoliosSucursal,
     }
 })();
 
