50db0badc3e4ba3a11237af0d9c9721ba469d40a
alejandro-jalf <aleflo_1996@outlook.com>
2022-03-28 11:00:46 -0600

    ADD: Agrega servicios para folios

--- src/components/General/validations/index.js
+++ src/components/General/validations/index.js
@@ -9,8 +9,34 @@ const validationGeneral = (() => {
         return createContentAssert('Empresa valida');
     }
 
+    const validateSucursal = (sucursal = '') => {
+        if (
+            sucursal.toUpperCase() !== 'ZR' &&
+            sucursal.toUpperCase() !== 'VC' &&
+            sucursal.toUpperCase() !== 'OU' &&
+            sucursal.toUpperCase() !== 'JL' &&
+            sucursal.toUpperCase() !== 'SA' &&
+            sucursal.toUpperCase() !== 'SB' &&
+            sucursal.toUpperCase() !== 'SU' &&
+            sucursal.toUpperCase() !== 'MA' &&
+            sucursal.toUpperCase() !== 'RE' &&
+            sucursal.toUpperCase() !== 'EN' &&
+            sucursal.toUpperCase() !== 'BO'
+        ) return createContentError('La sucursal no es valida');
+        return createContentAssert('Sucursal valida');
+    }
+
+    const validateNumber = (number = 0) => {
+        number = parseInt(number);
+        if (number.toString() === 'NaN') return createContentError('Envio un valor no numerico');
+        if (number <= 0) return createContentError('No puede ser menor que o igual a 0');
+        return createContentAssert('Numero valido');
+    }    
+
     return {
+        validateNumber,
         validateEmpresa,
+        validateSucursal,
     }
 })();
 
