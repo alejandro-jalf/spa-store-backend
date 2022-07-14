25cf66bea1f7d3ed4a83e42009722126ba5c388d
alejandro-jalf <aleflo_1996@outlook.com>
2022-07-14 18:47:05 -0500

    ADD: Agrega campos de factor compra y rel a detalles consolidacion

--- src/components/consolidaciones/models/index.js
+++ src/components/consolidaciones/models/index.js
@@ -23,7 +23,8 @@ const modelsConsolidaciones = (() => {
                     CostoUnitarioNetoUCIva = CASE WHEN Categoria = '02' THEN CostoUnitarioNetoUC * 1.16 ELSE CostoUnitarioNetoUC END,
                     CostoConIva = CASE WHEN Categoria = '02' THEN CostoValorNeto * 1.16 ELSE CostoValorNeto END,
                     Iva = CASE WHEN Categoria = '02' THEN CostoValorNeto * 0.16 ELSE 0 END,
-                    Tasa = CASE WHEN Categoria = '02' THEN '16.00' ELSE '0.00' END
+                    Tasa = CASE WHEN Categoria = '02' THEN '16.00' ELSE '0.00' END,
+                    UnidadCompra, Rel = CAST(CAST(FactorCompra AS int) AS nvarchar) + '/' + CAST(CAST(FactorVenta AS int) AS nvarchar)
                 FROM QVDEMovAlmacen
                 WHERE Documento = '${documento}'
                 `,
