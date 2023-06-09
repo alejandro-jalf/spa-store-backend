84d3ba72bd5a58edc0adcb86c735c8ddbc95dbb3
alejandro-jalf <aleflo_1996@outlook.com>
2023-06-09 10:20:41 -0600

    ADD: Agrega modelos restantes de solicitud de articulos

--- src/components/SolicitudArticulos/models/index.js
+++ src/components/SolicitudArticulos/models/index.js
@@ -74,14 +74,14 @@ const modelsPedidos = (() => {
     const updateSolicitud = async (cadenaConexion = '', uuid = '', body = {}) => {
         try {
             const {
-                CodigoBarra, Articulo, Nombre, IVA, Ieps, TazaIeps, TipoModelo, Marca, Presentacion,
+                CodigoBarra, Nombre, IVA, Ieps, TazaIeps, TipoModelo, Marca, Presentacion,
                 UnidadMedida, UnidadCompra, FactorCompra, UnidadVenta, FactorVenta, ActualizadoPor
             } = body;
             const accessToDataBase = dbmssql.getConexion(cadenaConexion);
             const result = await accessToDataBase.query(
                 `
                 UPDATE SolicitudArticulos SET
-                    CodigoBarra = '${CodigoBarra}', Articulo = '${Articulo}', Nombre = '${Nombre}', IVA = ${IVA}, Ieps = ${Ieps},
+                    CodigoBarra = '${CodigoBarra}', Nombre = '${Nombre}', IVA = ${IVA}, Ieps = ${Ieps},
                     TazaIeps = ${TazaIeps}, TipoModelo = '${TipoModelo}', Marca = '${Marca}', Presentacion = ${Presentacion},
                     UnidadMedida = '${UnidadMedida}', UnidadCompra = '${UnidadCompra}', FactorCompra = ${FactorCompra},
                     UnidadVenta = '${UnidadVenta}', FactorVenta = ${FactorVenta}, FechaActualizado = GETDATE(),
@@ -100,227 +100,39 @@ const modelsPedidos = (() => {
         }
     }
 
-    const getPedidosBySucursal = async (cadenaConexion = '', database = '', sucursal = '') => {
+    const updateStatus = async (cadenaConexion = '', uuid = '', estatus = '', Articulo = '') => {
         try {
             const accessToDataBase = dbmssql.getConexion(cadenaConexion);
             const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE ListaPedidosSucursal @Sucursal = '${sucursal}'`,
-                QueryTypes.SELECT
+                `
+                UPDATE SolicitudArticulos SET
+                    Estatus = '${estatus}', Articulo = '${Articulo}'
+                WHERE UUID = '${uuid}';
+                `,
+                QueryTypes.UPDATE
             );
             dbmssql.closeConexion();
-            return createContentAssert('Datos encontrados en la base de datos', result[0]);
+            return createContentAssert('Resultado de actualizar estatus', result[0]);
         } catch (error) {
             return createContentError(
-                'Fallo la conexion con base de datos al intentar obtener los pedidos por sucursal',
+                'Fallo la conexion con base de datos al intentar actualizar el estatus de la solicitud',
                 error
             );
         }
     }
 
-    const getListaArticulosByArticulo = async (cadenaConexion = '', database = '', articulo = '', folio = '', sucursal = '') => {
+    const deleteSolicitud = async (cadenaConexion = '', uuid = '') => {
         try {
             const accessToDataBase = dbmssql.getConexion(cadenaConexion);
             const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE ListaExistenciasAyudaArticulos @Busqueda = '${articulo}',
-                    @Sucursal = '${sucursal}',
-                    @Folio = ${folio}`,
-                QueryTypes.SELECT
+                `DELTE FROM SolicitudArticulos WHERE UUID = '${uuid}' AND Estatus = 'CANCELADO';`,
+                QueryTypes.DELETE
             );
             dbmssql.closeConexion();
-            return createContentAssert('Datos encontrados en la base de datos', result[0]);
+            return createContentAssert('Resultado de eliminar', result);
         } catch (error) {
             return createContentError(
-                'Fallo la conexion con base de datos al intentar obtener los articulos por articulo',
-                error
-            );
-        }
-    }
-
-    const getListaArticulosByNombre = async (cadenaConexion = '', database = '', nombre = '', folio = '', sucursal = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE ListaExistenciasAyuda @Busqueda = '${nombre}',
-                    @Sucursal = '${sucursal}',
-                    @Folio = ${folio}`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Datos encontrados en la base de datos', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar obtener los articulos por nombre',
-                error
-            );
-        }
-    }
-
-    const getListaArticulosByDias = async (cadenaConexion = '', database = '', sucursal = '', folio = '', dias = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE ListaExistenciasAyudaTop @Dias = ${dias},
-                    @Sucursal = '${sucursal}',
-                    @Folio = ${folio}`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Datos encontrados en la base de datos', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar obtener los articulos por dia',
-                error
-            );
-        }
-    }
-
-    const getReporteListaArticulos = async (cadenaConexion = '', database = '', sucursal = '', folio = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE ReporteListaArticulos @Sucursal = '${sucursal}', @Folio = ${folio}`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Datos encontrados en la base de datos', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar obtener el reporte de lista de articulos',
-                error
-            );
-        }
-    }
-
-    const getListaArticulos = async (cadenaConexion = '', database = '', sucursal = '', folio = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE VerListaArticulos @Sucursal = '${sucursal}', @Folio = ${folio}`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Datos encontrados en la base de datos', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar obtener la lista de articulos',
-                error
-            );
-        }
-    }
-
-    const addPedido = async (cadenaConexion = '', database = '', sucursal = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE AddPedidos @Sucursal = '${sucursal}'`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Pedido creado', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar agregar un nuevo pedido',
-                error
-            );
-        }
-    }
-
-    const addArticle = async (cadenaConexion = '', database = 'SPASUC2021', articulo = '', body = {}) => {
-        try {
-            const { pedido, sucursal, PeCaja, PePieza } = body;
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE RegistroPedidos @Articulo = '${articulo}',
-                    @Sucursal = '${sucursal}', @Pedido = '${pedido}',
-                    @PeCaja = ${PeCaja}, @PePieza = ${PePieza}`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Articulo agregado', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar agregar un articulo al pedido',
-                error
-            );
-        }
-    }
-
-    const sendPedido = async (cadenaConexion = '', database = '', sucursal = '', folio = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE SendPedidos @Sucursal = '${sucursal}', @Folio = ${folio}`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Pedido enviado', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar enviar el pedido',
-                error
-            );
-        }
-    }
-
-    const enProcesoPedido = async (cadenaConexion = '', database = '', sucursal = '', folio = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE EnProcesoPedidos @Sucursal = '${sucursal}', @Folio = ${folio}`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Pedido en proceso', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar poner en proceso el pedido',
-                error
-            );
-        }
-    }
-
-    const cancelPedido = async (cadenaConexion = '', database = '', sucursal = '', folio = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE CancelPedidos @Sucursal = '${sucursal}', @Folio = ${folio}`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Pedido Cancelado', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar cancelar el pedido',
-                error
-            );
-        }
-    }
-
-    const atendidoPedido = async (cadenaConexion = '', database = '', sucursal = '', folio = '', entrada = '', salida = '') => {
-        try {
-            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
-            const result = await accessToDataBase.query(
-                `USE ${database}
-                EXECUTE AtendidoPedidos @Sucursal = '${sucursal}', @Folio = ${folio},
-                    @Entrada = '${entrada}', @Salida = '${salida}'`,
-                QueryTypes.SELECT
-            );
-            dbmssql.closeConexion();
-            return createContentAssert('Pedido atendido', result[0]);
-        } catch (error) {
-            return createContentError(
-                'Fallo la conexion con base de datos al intentar atender el pedido',
+                'Fallo la conexion con base de datos al intentar eliminar la solicitud',
                 error
             );
         }
@@ -331,6 +143,8 @@ const modelsPedidos = (() => {
         getArticuloSolicitado,
         createSolicitud,
         updateSolicitud,
+        updateStatus,
+        deleteSolicitud,
     }
 })();
 
