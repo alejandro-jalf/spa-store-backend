34cac42f00fcfff9574c4cd52bda880efb2c40fd
alejandro-jalf <aleflo_1996@outlook.com>
2023-06-09 10:33:24 -0600

    ADD: Agrega validaciones y esquemas para solicitudes de articulos

--- src/components/SolicitudArticulos/schemas/index.js
+++ src/components/SolicitudArticulos/schemas/index.js
@@ -2,15 +2,25 @@ const joi = require('joi');
 
 const schemasPedidos = (() => {
 
-    const schemaCreateDetallesPedidos = joi.object({
-        pedido: joi.string().min(1).max(10).required(),
-        sucursal: joi.string().min(3).max(20).required(),
-        PeCaja: joi.number().min(0).required(),
-        PePieza: joi.number().min(0).required(),
+    const schemaUpdateRequest = joi.object({
+        CodigoBarra: joi.string().min(0).max(30).required(),
+        Nombre: joi.string().min(0).max(50).required(),
+        IVA: joi.number().min(0).max(1).required(),
+        Ieps: joi.number().min(0).max(1).required(),
+        TazaIeps: joi.number().required(),
+        TipoModelo: joi.string().min(0).max(30).required(),
+        Marca: joi.string().min(0).max(30).required(),
+        Presentacion: joi.number().required(),
+        UnidadMedida: joi.string().min(0).max(3).required(),
+        UnidadCompra: joi.string().min(0).max(3).required(),
+        FactorCompra: joi.number.required(),
+        UnidadVenta: joi.string().min(0).max(3).required(),
+        FactorVenta: joi.number().required(),
+        ActualizadoPor: joi.string().max(40).required(),
     });
 
     return {
-        schemaCreateDetallesPedidos,
+        schemaUpdateRequest,
     }
 })() ;
 
--- src/components/SolicitudArticulos/validations/index.js
+++ src/components/SolicitudArticulos/validations/index.js
@@ -1,4 +1,4 @@
-const { schemaCreateDetallesPedidos } = require('../schemas/index');
+const { schemaUpdateRequest } = require('../schemas/index');
 const { createContentAssert, createContentError } = require('../../../utils');
 
 const validationPedidos = (() => {
@@ -20,28 +20,29 @@ const validationPedidos = (() => {
         return createContentAssert('Sucursal valida');
     }
 
-    const validateBodyAddArticle = (bodyArticle = {}) => {
-        const resultValidate = schemaCreateDetallesPedidos.validate(bodyArticle)
+    const validateBodyUpdateRequest = (bodyRequest = {}) => {
+        const resultValidate = schemaUpdateRequest.validate(bodyRequest)
         if (resultValidate.error)
             return createContentError('Algun dato fue enviado de manera incorrecta', resultValidate.error);
         return createContentAssert('Body correcto');
     }
 
-    const validateStatusPedido = (status = '') => {
+    const validateStatusRequest = (status = '') => {
         if (status.trim() === '') return createContentError('Se recibio estatus vacio');
         if (
-            status.trim().toUpperCase() !== 'PEDIDO CANCELADO' &&
-            status.trim().toUpperCase() !== 'PEDIDO EN PROCESO' &&
-            status.trim().toUpperCase() !== 'PEDIDO ENVIADO' &&
-            status.trim().toUpperCase() !== 'PEDIDO ATENDIDO'
+            status.trim().toUpperCase() !== 'EN SUCURSAL' &&
+            status.trim().toUpperCase() !== 'EN PROCESO' &&
+            status.trim().toUpperCase() !== 'ENVIADO' &&
+            status.trim().toUpperCase() !== 'CANCELADO' &&
+            status.trim().toUpperCase() !== 'ATENDIDO'
         ) return createContentError('Estatus invalido');
         return createContentAssert('Estatus correcto');
     }
 
     return {
         validateSucursal,
-        validateStatusPedido,
-        validateBodyAddArticle,
+        validateStatusRequest,
+        validateBodyUpdateRequest,
     }
 })();
 
