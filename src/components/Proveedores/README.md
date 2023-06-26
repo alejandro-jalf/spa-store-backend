54a0e64d431f8b8998ec17b120f81deae8657030
alejandro-jalf <aleflo_1996@outlook.com>
2023-06-26 11:56:54 -0600

    ADD: Agrega modelos para solicitud de proveedores

--- src/components/Proveedores/README.md
+++ src/components/Proveedores/README.md
@@ -7,3 +7,43 @@ Componente para controlar las rutas relacionadas con la informacion y acciones n
 | **Metodo** | **Ruta** | **Request** | **Descripcion** |
 |------------|----------|-------------|-----------------|
 | **GET** | _/api/v1/proveedores_ |  | Obtiene Nombre e Id de los proveedores existentes |
+
+#### SolicitudProveedores
+
+| **Campo** | **Tipo** | **Tamaño** | **Descripcion** |
+|-----------|----------|------------|-----------------|
+| UUID | nvarchar | 100 | primary key |
+| CuentaContabilidad | nvarchar | 20 | |
+| CuentaWinCaja | nvarchar | 35 | |
+| Nombre | nvarchar | 200 | |
+| RFC | nvarchar | 30 |  |
+| Direccion | nvarchar | 200 | |
+| Telefono | nvarchar | 20 |  |
+| Correo | nvarchar | 50 |  |
+| Estatus | nvarchar | 20 |  |
+| FechaCreado | datetime | | |
+| CreadoPor | nvarchar | 50 | |
+| FechaEnviado | datetime | | |
+| FechaAtendido | datetime | |  |
+
+#### script sql
+
+```sql
+CREATE TABLE SolicitudProveedores(
+    UUID nvarchar(100) DEFAULT NEWID(),
+    Sucursal nvarchar(3),
+    Consecutivo int,
+    CuentaContabilidad nvarchar(20),
+    CuentaWinCaja nvarchar(35),
+    Nombre nvarchar(200),
+    RFC nvarchar(30),
+    Direccion nvarchar(200),
+    Telefono nvarchar(20),
+    Correo nvarchar(50),
+    Estatus nvarchar(20),
+    FechaCreado datetime DEFAULT GETDATE(),
+    CreadoPor nvarchar(50),
+    FechaEnviado datetime DEFAULT GETDATE(),
+    FechaAtendido datetime DEFAULT GETDATE()
+);
+```
--- src/components/Proveedores/models/index.js
+++ src/components/Proveedores/models/index.js
@@ -23,8 +23,184 @@ const modelsProveedores = (() => {
         }
     }
 
+    const getAllRequestProviders = async (cadenaConexion = '') => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                'USE CA2015; SELECT TOP 100 * FROM SolicitudProveedores ORDER BY FechaCreado DESC;',
+                QueryTypes.SELECT
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Solicitudes encontrados', result[0]);
+        } catch (error) {
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar obtener los proveedores',
+                error
+            );
+        }
+    }
+
+    const getRequestBySuc = async (cadenaConexion = '', sucursal = '') => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `USE CA2015;
+                SELECT TOP 100 * FROM SolicitudProveedores WHERE Sucursal = '${sucursal}' ORDER BY FechaCreado DESC;
+                `,
+                QueryTypes.SELECT
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Lista de solicitudes por sucursal', result[0]);
+        } catch (error) {
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar obtener la Lista de solicitudes por sucursal',
+                error
+            );
+        }
+    }
+
+    const getRequestProvider = async (cadenaConexion = '', uuid) => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `
+                USE CA2015;
+                SELECT * FROM SolicitudProveedores WHERE UUID = '${uuid}';
+                `,
+                QueryTypes.SELECT
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('articulo solicitado', result[0]);
+        } catch (error) {
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar obtener la articulo solicitado',
+                error
+            );
+        }
+    }
+
+    const createRequestProvider = async (cadenaConexion = '', sucursal= '', CreadoPor = '') => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `
+                USE CA2015;
+                DECLARE @Consecutivo int = (SELECT TOP 1 Consecutivo FROM SolicitudProveedores WHERE Sucursal = '${sucursal}' ORDER BY Consecutivo DESC);
+                DECLARE @NewConsecutivo int = ISNULL(@Consecutivo, 0);
+
+                INSERT INTO SolicitudProveedores(
+                    Sucursal, Consecutivo, CuentaContabilidad, CuentaWinCaja, Nombre, RFC, Direccion, Telefono, Correo, Estatus, CreadoPor
+                ) VALUES (
+                    '${sucursal}', @NewConsecutivo + 1, '', '', '', '', '', '', '', 'EN SUCURSAL', '${CreadoPor}'
+                );
+                SELECT *  FROM SolicitudProveedores WHERE Sucursal = '${sucursal}' AND Consecutivo = @NewConsecutivo + 1;
+                `,
+                QueryTypes.SELECT
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Proveedores encontrados', result[0]);
+        } catch (error) {
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar obtener los proveedores',
+                error
+            );
+        }
+    }
+
+    const updateRequestProvider = async (cadenaConexion = '', uuid = '', body = {}) => {
+        try {
+            const { Nombre, RFC, Direccion, Telefono, Correo } = body;
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `
+                USE CA2015;
+                UPDATE SolicitudProveedores SET
+                    Nombre = '${Nombre}', RFC = '${RFC}', Direccion = '${Direccion}', Telefono = '${Telefono}', Correo = '${Correo}'
+                WHERE UUID = '${uuid}';
+                `,
+                QueryTypes.UPDATE
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Resultado de actualizacion', result);
+        } catch (error) {
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar actualizar los datos de la solicitud',
+                error
+            );
+        }
+    }
+
+    const updateRequestCount = async (cadenaConexion = '', uuid = '', CuentaContabilidad = '', CuentaWinCaja = '') => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `
+                USE CA2015;
+                UPDATE SolicitudProveedores SET
+                    CuentaContabilidad = '${CuentaContabilidad}', CuentaWinCaja = '${CuentaWinCaja}', Estatus = '${Estatus}'
+                WHERE UUID = '${uuid}';
+                `,
+                QueryTypes.UPDATE
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Resultado de actualizacion', result);
+        } catch (error) {
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar actualizar los datos de la solicitud',
+                error
+            );
+        }
+    }
+
+    const updateRequestStatus = async (cadenaConexion = '', uuid = '', estatus = '', Articulo = '') => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `
+                USE CA2015;
+                UPDATE SolicitudProveedores SET
+                    Estatus = '${estatus}'
+                WHERE UUID = '${uuid}';
+                `,
+                QueryTypes.UPDATE
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Resultado de actualizar estatus', result);
+        } catch (error) {
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar actualizar el estatus de la solicitud',
+                error
+            );
+        }
+    }
+
+    const deleteRequest = async (cadenaConexion = '', uuid = '') => {
+        try {
+            const accessToDataBase = dbmssql.getConexion(cadenaConexion);
+            const result = await accessToDataBase.query(
+                `USE CA2015; DELETE FROM SolicitudProveedores WHERE UUID = '${uuid}' AND Estatus = 'CANCELADO';`,
+                QueryTypes.DELETE
+            );
+            dbmssql.closeConexion();
+            return createContentAssert('Resultado de eliminar', result);
+        } catch (error) {
+            return createContentError(
+                'Fallo la conexion con base de datos al intentar eliminar la solicitud',
+                error
+            );
+        }
+    }
+
     return {
         getAllProviders,
+        getAllRequestProviders,
+        getRequestBySuc,
+        getRequestProvider,
+        createRequestProvider,
+        updateRequestProvider,
+        updateRequestCount,
+        updateRequestStatus,
+        deleteRequest,
     }
 })();
 
