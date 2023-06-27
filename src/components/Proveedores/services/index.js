e35d9267340a56b4ce72543a09423d1c61b66576
alejandro-jalf <aleflo_1996@outlook.com>
2023-06-26 18:19:55 -0600

    ADD: Agrega servicio para obtener solicitudes de proveedores

--- src/components/Proveedores/services/index.js
+++ src/components/Proveedores/services/index.js
@@ -6,9 +6,13 @@ const { validateSucursal } = require('../validations');
 const {} = require('../utils');
 const {
     getAllProviders,
+    getAllRequestProviders,
+    getRequestProvider,
+    getRequestBySuc,
 } = require('../models');
 
 const ServicesProveedores = (() => {
+    const conexionSol = getConnectionFrom('ZR');
 
     const getProveedores = async (sucursal = 'BO') => {
         sucursal = sucursal.toUpperCase().trim() === 'ALL' ? 'ZR' : sucursal;
@@ -22,8 +26,27 @@ const ServicesProveedores = (() => {
         return createResponse(200, response)
     }
 
+    const getSolicitudesProveedor = async (sucursal) => {
+        let validate = validateSucursal(sucursal);
+        if (!validate.success) return createResponse(400, validate);
+
+        const response = sucursal === 'ALL' ? await getAllRequestProviders(conexionSol) : await getRequestBySuc(conexionSol, sucursal);
+
+        if (!response.success) return createResponse(400, response);
+        return createResponse(200, response)
+    }
+
+    const getSolicitudProveedor = async (uuid = '') => {
+        const response = await getArticuloSolicitado(conexionSol, uuid);
+
+        if (!response.success) return createResponse(400, response);
+        if (response.data.length === 0) return createResponse(200, createContentError('No existe esta solicitud'))
+        return createResponse(200, response)
+    }
+
     return {
         getProveedores,
+        getSolicitudesProveedor,
     }
 })();
 
